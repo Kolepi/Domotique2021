2aea71899263041482ccba79a8d31db428935f81
Ilyas-Boulahfa <76109921+Ilyas-Boulahfa@users.noreply.github.com>
2021-04-20 16:33:42 +0200

    encore le boss

--- php/inscription.php
+++ php/inscription.php
@@ -6,12 +6,20 @@ require_once "bdd.php";
 header("content-type:application/json; charset=utf-8");
 
 //Creation de la requete SQL
-$sql = "INSERT INTO utilisateur (`username`, `mail`, `password`) VALUES(?,?,?)";
+$sql = "INSERT INTO utilisateur (`username`, `mail`, `password`,`secret`) VALUES(?,?,?,?)";
 //Valeurs d'insertion
-$query = executeSQL($Bdd,$sql,array($_POST["username"],$_POST["email"],$_POST["password"]));
-
+try {
+    $query = executeSQL($Bdd,$sql,array($_POST["username"],$_POST["email"],$_POST["password"],$_POST["secret"]));
+}
+catch (\Exception $e) {
+    $messageerr = getErrorCode($e->getMessage());
+    echo json_encode(array(
+        "errorCode" =>$messageerr[1],
+        "message" => $messageerr[2] 
+    ));
+    die();
+}
 //DEBUG
 $result=$Bdd->lastinsertid();
 
 echo json_encode($result > 0);
-?>
