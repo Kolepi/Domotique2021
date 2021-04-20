d301896d1bce4d09818fe0eb1c18c747a3380a59
Ilyas-Boulahfa <76109921+Ilyas-Boulahfa@users.noreply.github.com>
2021-04-20 15:07:14 +0200

    boss

--- php/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//Lien avec la bdd
-require_once "bdd.php";
-
-header("content-type:application/json; charset=utf-8");
-
-//Creation de la requete SQL
-$sql = "DELETE 
--- php/inscription.php
+++ php/inscription.php
@@ -8,10 +8,18 @@ header("content-type:application/json; charset=utf-8");
 //Creation de la requete SQL
 $sql = "INSERT INTO utilisateur (`username`, `mail`, `password`) VALUES(?,?,?)";
 //Valeurs d'insertion
-$query = executeSQL($Bdd,$sql,array($_POST["username"],$_POST["email"],$_POST["password"]));
-
+try {
+    $query = executeSQL($Bdd,$sql,array($_POST["username"],$_POST["email"],$_POST["password"]));
+}
+catch (\Exception $e) {
+    $messageerr = getErrorCode($e->getMessage());
+    echo json_encode(array(
+        "errorCode" =>$messageerr[1],
+        "message" => $messageerr[2] 
+    ));
+    die();
+}
 //DEBUG
 $result=$Bdd->lastinsertid();
 
 echo json_encode($result > 0);
-?>
